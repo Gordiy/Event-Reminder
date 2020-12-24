8761eb2a8342bbf8f3feaba25ca38226044847ae
Gordiy Rushynets <gordiyrushynets@Gordiys-MacBook-Pro.local>
2020-12-24 04:10:26 +0200

    created connection with database and tables in db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
-var express = require('express');
+const express = require('express');
+const { Client } = require('pg')
 
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'gordiyrushynets',
+    database: 'event_reminder',
+    password: '',
+})
+
+client.connect()
+
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    client.end()
+})
 
 const app = express();
 const server = require("http").Server(app);
