add0aecee18cec531018b980ce1ce84e66aae4d4
Gordiy Rushynets <gordiyrushynets@Gordiys-MacBook-Pro.local>
2020-12-24 02:17:30 +0200

    created project

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+
+const app = express();
+const server = require("http").Server(app);
+
+const rooms = new Map();
+
+
+
+app.get('/home', (req, resp)=>{
+    resp.send('hello')
+})
+
+server.listen(8000, (err) => {
+    if (err) {
+        throw Error(err);
+    }
+    console.log("Server started.")
+})
