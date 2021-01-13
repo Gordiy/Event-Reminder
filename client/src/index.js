5e96625bb643d18a8040278f16970e53f2fbb02c
Gordiy Rushynets <gordiyrushynets@Gordiys-MacBook-Pro.local>
2021-01-13 20:52:48 +0200

    added ability to show pop up window when press on day

--- client/src/index.js
+++ client/src/index.js
@@ -5,21 +5,56 @@ import './index.css'
 import 'react-calendar/dist/Calendar.css';
 import reportWebVitals from './reportWebVitals';
 import Calendar from 'react-calendar';
-import Popup from 'reactjs-popup';
+// import Popup from 'reactjs-popup';
 
+class Popup extends React.Component {
+	render() {
+	  return (
+		<div className='popup'>
+		  <div className='popup_inner'>
+			<h1>{this.props.text}</h1>
+		  <button onClick={this.props.closePopup}>close me</button>
+		  </div>
+		</div>
+	  );
+	}
+  }
 
 class EventCalendar extends React.Component {
-	constructor() {
-		super();
-		this._state = false;
+	constructor(props) {
+		super(props);
+		this.state = {
+			showPopup: true
+		  };
+
+		// this.onDayClick = this.onDayClick.bind(this);
+		this.togglePopup = this.togglePopup.bind(this);
 	}
 
-	onClickDay() {
-		this._state = !this._state;
+	togglePopup() {
+		this.setState({
+		  showPopup: !this.state.showPopup
+		});
+	  }
+
+	onDayClick(e) {
+		console.log(e)
+		this.togglePopup.bind(this);
 	}
 
 	render() {
-		return <Calendar onClickDay />
+		return(
+				<div className='app'>
+					<Calendar onClickDay={this.togglePopup} />
+					{this.state.showPopup ? 
+						<Popup
+							text='Close Me'
+							closePopup={this.togglePopup.bind(this)}
+						/>
+						: null
+					}
+				</div>
+			)
 	}
 }
 
@@ -74,9 +109,6 @@ const Modal = () => (
   </Popup>
 );
 
-const show = () => {
-	
-}
 
 const ReactCalendar = () => {
 	const [date, setDate] = useState(new Date());
@@ -95,8 +127,7 @@ const ReactCalendar = () => {
 
 ReactDOM.render(
 	<div>
-		<ReactCalendar />
-		<Modal/>
+		<EventCalendar />
 	</div>,
   document.querySelector('#root')
 );
