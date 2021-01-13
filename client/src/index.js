8fe3971b419db27df7da4fc442598eb3c185b960
Gordiy Rushynets <gordiyrushynets@Gordiys-MacBook-Pro.local>
2021-01-13 16:39:31 +0200

    added pop up window

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,82 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 import 'react-calendar/dist/Calendar.css';
 import reportWebVitals from './reportWebVitals';
 import Calendar from 'react-calendar';
+import Popup from 'reactjs-popup';
+
+
+class EventCalendar extends React.Component {
+	constructor() {
+		super();
+		this._state = false;
+	}
+
+	onClickDay() {
+		this._state = !this._state;
+	}
+
+	render() {
+		return <Calendar onClickDay />
+	}
+}
+
+const Modal = () => (
+  <Popup
+	open={true}
+    // trigger={<button className="button"> Open Modal </button>}
+    modal
+    nested
+  >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Modal Title </div>
+        <div className="content">
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+        </div>
+        <div className="actions">
+          <Popup
+            trigger={<button className="button"> Trigger </button>}
+            position="top center"
+            nested
+          >
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+              magni omnis delectus nemo, maxime molestiae dolorem numquam
+              mollitia, voluptate ea, accusamus excepturi deleniti ratione
+              sapiente! Laudantium, aperiam doloribus. Odit, aut.
+            </span>
+          </Popup>
+          <button
+            className="button"
+            onClick={() => {
+              console.log('modal closed ');
+              close();
+            }}
+          >
+            close modal
+          </button>
+        </div>
+      </div>
+    )}
+  </Popup>
+);
+
+const show = () => {
+	
+}
 
 const ReactCalendar = () => {
 	const [date, setDate] = useState(new Date());
@@ -21,7 +94,10 @@ const ReactCalendar = () => {
 }
 
 ReactDOM.render(
-  <ReactCalendar/>,
+	<div>
+		<ReactCalendar />
+		<Modal/>
+	</div>,
   document.querySelector('#root')
 );
 
